a67be603c2bdb533d7a0be2979400f1db04c9df3
Michal Grzadkowski <mgrzad@gmail.com>
2017-05-31 22:40:38 -0700

    adding PNLL RPPA data, environment package requirements

---
+++ HetMan/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(name='HetMan',
+      version='0.1',
+      description='Mutation Heterogeneity Manifold',
+      author='Michal Grzadkowski',
+      author_email='grzadkow@ohsu.edu',
+      package_dir={'HetMan': ''},
+      packages=['HetMan'],
+     )
+
