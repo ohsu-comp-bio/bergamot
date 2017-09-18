e1c449461efc5a26ac545b3962688cbcf1e758a0
Michal Grzadkowski <mgrzad@gmail.com>
2017-09-18 14:26:39 -0700

    First running version of DREAM stan fitting experiment, updating how TransferDreamCohort chooses genetic features.

--- HetMan/experiments/dream_train/stan/run.py
+++ HetMan/experiments/dream_train/stan/run.py
@@ -1,24 +1,48 @@
 
-# use your own Synapse cache and credentials here
+import os
+base_dir = os.path.dirname(__file__)
+
+import sys
+sys.path.extend([os.path.join(base_dir, '../../../..')])
+
 import synapseclient
-syn = synapseclient.Synapse()
-syn.login()
+import dill as pickle
 
-# loads the challenge data
 from HetMan.features.cohorts import TransferDreamCohort
-cdata = TransferDreamCohort(
-    syn, 'BRCA', intx_types=['controls-expression-of'],
-    cv_seed=34, cv_prop=0.8
-    )
-
-# initializes the model and fits it using all of the genes in the
-# `inter`section of the RNA genes, CNA genes, and proteome genes
 import HetMan.predict.bayesian_pathway.multi_protein as mpt
-clf = mpt.StanProteinPipe('controls-expression-of')
 
-# finds the best combination of model hyper-parameters, uses these
-# parameters to fit to the data
-clf.tune_coh(cdata, pheno='inter',
-             tune_splits=3, test_count=4, parallel_jobs=12)
-clf.fit_coh(cdata, pheno='inter') 
+
+def main(argv):
+
+    # use your own Synapse cache and credentials here
+    syn = synapseclient.Synapse()
+    syn.cache.cache_root_dir = ("/home/exacloud/lustre1/CompBio/"
+                                "mgrzad/input-data/synapse")
+    syn.login()
+
+    # loads the challenge data
+    cdata = TransferDreamCohort(
+        syn, argv[0], intx_types=[argv[1]],
+        cv_seed=171, cv_prop=0.8, miss_cutoff=0.1
+        )
+
+    out_dir = os.path.join(base_dir, 'output')
+    out_file = os.path.join(out_dir, argv[0], argv[1], 'results', 'coefs.p')
+
+    # initializes the model and fits it using all of the genes in the
+    # `inter`section of the RNA genes, CNA genes, and proteome genes
+    clf = mpt.StanProteinPipe(argv[1])
+
+    # finds the best combination of model hyper-parameters, uses these
+    # parameters to fit to the data
+    clf.tune_coh(cdata, pheno='inter',
+                 tune_splits=4, test_count=4, parallel_jobs=16)
+    clf.fit_coh(cdata, pheno='inter') 
+
+    clf.eval_coh(cdata, pheno='inter')
+    pickle.dump(clf, open(out_file, 'wb'))
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
 
