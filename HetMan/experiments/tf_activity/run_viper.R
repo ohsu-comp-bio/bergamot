91006f320355ac325b471c4ceef902a394e34d54
Hannah Manning <manningh@ohsu.edu>
2017-09-21 17:16:53 -0700

    Reshuffled for entrez-to-ensembl ARACNe regulatory network ID mapping

--- HetMan/experiments/tf_activity/run_viper.R
+++ HetMan/experiments/tf_activity/run_viper.R
@@ -23,19 +23,20 @@ main <- function() {
   args <- commandArgs(trailingOnly = TRUE)
   # expr_fl should be the filename (not path) for matrix
   # with samples as cols and features as rows
-  expr_fl <- paste('./', args[2])
-  phen_fl <- paste('./', args[3])
+  expr_fl <- paste('./', args[2], sep="")
+  phen_fl <- paste('./', args[3], sep="")
   cohort <- args[4]
   library("viper")
   library("Biobase")
   library(aracne.networks)
 
-  # load expression data
+  print("Loading expression data in R")
   # exprDataPath <- file.path(datadir, expr_fl)
   expr <- as.matrix(read.table(expr_fl, row.names=1,
                                header=TRUE, sep="\t",
                                as.is=TRUE, check.names=FALSE))
   
+  print("Loading phenotype data in R")
   # load phenotyptic data (will be used to separate expr into experimental groups)
   pdat <- read.table(phen_fl, row.names=1, 
                       header=FALSE, sep="\t")
@@ -47,42 +48,20 @@ main <- function() {
   # turn it into an AnnotatedDataFrame as ExpressionSet requires
   pData <- AnnotatedDataFrame(data=pdat)
   
+  print("Generating Biobase ExpressionSet")
   # build a Biobase ExpressionSet
   xset <- ExpressionSet(assayData=expr, phenoData=pData)
   
-  # build regulon selecting structure
-  aracne_regs <- c("regulonblca", "regulonbrca", "reguloncesc", 
-                   "reguloncoad", "regulonesca", "regulongbm",
-                   "regulonhnsc", "regulonkirc", "regulonkirp", 
-                   "regulonlaml", "regulonlihc", "regulonluad", 
-                   "regulonlusc", "regulonov", "regulonpaad",
-                   "regulonpcpg", "regulonprad", "regulonread", 
-                   "regulonsarc", "regulonstad", "regulontgct", 
-                   "regulonthca", "regulonthym", "regulonucec")
+  # specify name of regulon file with ENSEMBL ids
+  reg_fl=paste('./', 'tmp-ensembl-', cohort, '-reg.adj', sep="")
+  print("Regulatory network with ensembl IDs will be loaded from:")
+  print(gsub('./', '', reg_fl))
   
-  names(aracne_regs) <- c("TCGA-BLCA", "TCGA-BRCA", "TCGA-CESC",
-                          "TCGA-COAD", "TCGA-ESCA", "TCGA-GBM",
-                          "TCGA-HNSC", "TCGA-KIRC", "TCGA-KIRP",
-                          "TCGA-LAML", "TCGA-LIHC", "TCGA-LUAD",
-                          "TCGA-LUSC", "TCGA-ONOV", "TCGA-PAAD",
-                          "TCGA-PCPG", "TCGA-PRAD", "TCGA-READ",
-                          "TCGA-SARC", "TCGA-STAD", "TCGA-TCGT",
-                          "TCGA-THCA", "TCGA-THYM", "TCGA-UCEC")
-  
-  # select aracne regulatory network name for cohort of interest
-  regname <- aracne_regs[[cohort]]
-  
-  # load the regulatory network
-  reg_network = get(regname)
-  
-  # write out the regulatory network because aracne2regulon expects a file.
-  reg_fl=gsub('pData.tsv', 'reg.adj', phen_fl)
-  write.regulon(reg_network, file=reg_fl)
-  
+  # UNCOMMENT THIS WHEN ENTREZ-ENSEMBL MAPPING IS COMPLETE
   # load the regulatory network into regulon object
-  # TODO: map our ensembl ids to their entrez ones
-  regul <- aracne2regulon(reg_fl, xset, verbose=FALSE)
+  # regul <- aracne2regulon(reg_fl, xset, verbose=FALSE)
   
+  print("Calculating signature")
   # TODO: ADAPT SAMPLE TYPES TO BE REPRESENTATIVE OF ALL IN TCGA COHORTS
   # calculate signatures (vector of t statistics, then transform to Z scores)
   signature <- rowTtest(xset, "samp_type", c("Primary_Tumor", "Metastatic"), 
@@ -90,13 +69,14 @@ main <- function() {
   signature <- (qnorm(signature$p.value/2, lower.tail = FALSE) *
                   sign(signature$statistic))[,1]
   
+  # UNCOMMENT THIS SECTION WHEN ENTREZ-ENSEMBL MAPPING IS COMPLETE 
   # generate a null model for the signature to be compared against
-  nullmodel <- ttestNull(xset, "samp_type", c("Primary_Tumor", "Metastatic"), 
-                         "Solid_Tissue_Normal", per = 1000,
-                         repos = TRUE, verbose = FALSE)
-  
-  # generate activity scores based on multiple samples
-  mrs <- msviper(signature, regul, nullmodel, verbose = FALSE)
+  # nullmodel <- ttestNull(xset, "samp_type", c("Primary_Tumor", "Metastatic"), 
+  #                        "Solid_Tissue_Normal", per = 1000,
+  #                        repos = TRUE, verbose = FALSE)
+  # 
+  # # generate activity scores based on multiple samples
+  # mrs <- msviper(signature, regul, nullmodel, verbose = FALSE)
   
   # view results:
   # summary(mrs)
@@ -107,3 +87,5 @@ main <- function() {
   # get t statistic and p value for difference between the categorizations of B cells
   # tmp <- rowTtest(vpres, "Group", c(cohort, "Normal))
 }
+
+main()
--- HetMan/experiments/tf_activity/run_viper.sh
+++ HetMan/experiments/tf_activity/run_viper.sh
@@ -17,16 +17,28 @@ while getopts "c:a:p:" opt; do
 done
 
 shift $((OPTIND-1))
-echo "cohort=$cohort, adjfile=$adjfile"
+echo "Preparing to run VIPER on $cohort"
 
 exprfile=tmp-$cohort-expression.tsv
 pfile=tmp-$cohort-pData.tsv
 
-echo "exprfile=$exprfile, pfile=$pfile"
+echo "$cohort expression and sample_type (i.e. Tumor v. Normal) will be "
+echo "temporarily saved in ../../data/tf_activity/ as:"
+echo "$exprfile and $pfile"
 
 # get and write out the expression file
 # capture the names of that file as expr
+echo "Running prep_for_viper.py -c $cohort"
 python prep_for_viper.py -c $cohort
 
+echo "Loading and saving ARACNe regulatory network derived from $cohort context"
+Rscript write_aracne_regs.R $cohort
+
+echo "Mapping regulatory network's entrez IDs to ensembl IDs"
+python joeys_translator.py -c $cohort
+
 # Run VIPER using the expression data and regulon relationships
+echo "Running run_viper.R $exprfile $pfile $cohort"
 Rscript run_viper.R $exprfile $pfile $cohort
+
+echo "Mission accomplished"
