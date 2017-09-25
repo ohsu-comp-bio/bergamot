4f730780bfa19cac7beeb8bc13b5cef5a84f8e23
Michal Grzadkowski <mgrzad@gmail.com>
2017-09-25 15:27:12 -0700

    Improving subvariant partition optimizer trace messages, removing recursive branch traversal.

--- HetMan/experiments/subvariant_partition/fit.py
+++ HetMan/experiments/subvariant_partition/fit.py
@@ -37,36 +37,52 @@ import synapseclient
 
 
 class PartitionOptim(object):
+    """A class for finding the optimal partition of a gene's sub-types.
+
+    Attributes:
+        cdata (VariantCohort): A set of tumour samples with expression and
+        variant call data, split into training and testing cohorts.
+        pred_scores (pd.DataFrame)
+        mtype_scores (dict)
+
+    Examples:
+        >>> PartitionOptim(cdata, MuType({('Gene', 'TP53'): None}),
+                           'TP53', Lasso)
+        >>> PartitionOptim(cdata, MuType({('Gene', 'CDH1'): None}),
+                           'CDH1', rForest)
+
+    """
 
     def __init__(self,
                  cdata, base_mtype, gene, clf,
-                 use_lvls=('Form', 'Exon', 'Protein')):
+                 use_lvls=('Form', 'Exon', 'Protein'), verbose=10):
 
         self.cdata = cdata
-        self.base_mtype = base_mtype
-        self.gene = gene
-
-        self.clf = clf
         self.pred_scores = pd.DataFrame(index=cdata.test_samps)
+        self.mtype_scores = {}
 
+        self.base_mtype = base_mtype
         self.base_train_samps = base_mtype.get_samples(cdata.train_mut)
         self.base_test_samps = base_mtype.get_samples(cdata.test_mut)
+
+        self.gene = gene
+        self.clf = clf
+        self.verbose = verbose
 
         self.use_lvls = use_lvls
         self.lvl_index = 0
 
-        self.mtype_scores = {}
         self.cur_mtype = base_mtype
         self.next_mtypes = []
         self.back_mtypes = []
-        self.best_mtypes = []
+        self.best_mtypes = set()
 
     def __str__(self):
-        return ("Current mtype: {}\n"
-                "Current level: {}".format(
+        return ("\nThe current state of the optimizer is:\n"
+                "\tCurrent mtype: {}\n\tCurrent level: {}".format(
                     self.cur_mtype, self.use_lvls[self.lvl_index]))
 
-    def score_mtypes(self, mtypes, verbose=False):
+    def score_mtypes(self, mtypes):
         """Scores a list of mutation sub-types by classification accuracy."""
 
         out_scores = {mtype: {'All': 0, 'Null': 0} for mtype in mtypes}
@@ -123,14 +139,17 @@ class PartitionOptim(object):
                         )
                     ]
             
-            if verbose:
-                print(mtype)
-                print(round(out_scores[mtype]['All'], 4))
-                print(round(out_scores[mtype]['Null'], 4))
+            if self.verbose > 1:
+                print("Testing {} :"
+                      "\n\tAUC using all samples:\t\t{:.4f}"
+                      "\n\tAUC w/o other mutated samples:\t{:.4f}"
+                        .format(mtype,
+                                out_scores[mtype]['All'],
+                                out_scores[mtype]['Null']))
 
         return out_scores
 
-    def get_null_performance(self, draw_count=50):
+    def get_null_performance(self, draw_count=5):
         """Estimates the null score distribution for the current sub-type.
 
         Args:
@@ -173,66 +192,83 @@ class PartitionOptim(object):
 
     def get_clf_projection(self, mtypes):
         cur_samps = self.cur_mtype.get_samples(self.cdata.test_mut)
-        mtype_proj = {mtype: -1 for mtype in mtypes}
+        mtype_proj = {mtype: 0 for mtype in mtypes}
 
         for mtype in mtypes:
             mtype_samps = mtype.get_samples(self.cdata.test_mut)
 
-            if mtype_samps != cur_samps:
-                left_scores = self.pred_scores.loc[
-                    cur_samps - mtype_samps, (mtype, )]
-                null_scores = self.pred_scores.loc[
-                    self.cdata.test_samps - self.base_test_samps, (mtype, )]
-                mtype_scores = self.pred_scores.loc[mtype_samps, (mtype, )]
+            left_scores = self.pred_scores.loc[
+                cur_samps - mtype_samps, (mtype, )]
+            null_scores = self.pred_scores.loc[
+                self.cdata.test_samps - self.base_test_samps, (mtype, )]
+            mtype_scores = self.pred_scores.loc[mtype_samps, (mtype, )]
 
-                null_param = norm.fit(null_scores)
-                mtype_param = norm.fit(mtype_scores)
+            null_param = norm.fit(null_scores)
+            mtype_param = norm.fit(mtype_scores)
 
-                left_null_likl = np.sum(
-                    norm.logpdf(left_scores, *null_param))
-                left_mtype_likl = np.sum(
-                    norm.logpdf(left_scores, *mtype_param))
+            left_null_likl = np.sum(norm.logpdf(left_scores, *null_param))
+            left_mtype_likl = np.sum(norm.logpdf(left_scores, *mtype_param))
 
-                mtype_proj[mtype] = left_mtype_likl - left_null_likl
+            mtype_proj[mtype] = left_mtype_likl - left_null_likl
 
         return mtype_proj
 
-    def step(self, verbose=False):
-        if self.cur_mtype == self.base_mtype:
-            self.mtype_scores.update(self.score_mtypes(
-                [self.base_mtype], verbose))
+    def step(self):
+        if self.cur_mtype == self.base_mtype and self.lvl_index == 0:
+            self.mtype_scores.update(self.score_mtypes([self.base_mtype]))
 
         test_mtypes = self.cdata.train_mut.combtypes(
             mtype=self.cur_mtype, sub_levels=[self.use_lvls[self.lvl_index]],
             comb_sizes=(1, 2, 3), min_type_size=10
             )
 
-        out_scores = self.score_mtypes(test_mtypes, verbose)
+        # filters out sub-types that are similar enough to the current type
+        # that they have the same sample set in the testing cohort
+        test_mtypes = [
+            mtype for mtype in test_mtypes
+            if (mtype.get_samples(self.cdata.test_mut)
+                != self.cur_mtype.get_samples(self.cdata.test_mut))
+            ]
+
+        if self.verbose > 0:
+            print("\nFound {} sub-types to test...".format(len(test_mtypes)))
+
+        out_scores = self.score_mtypes(test_mtypes)
         self.mtype_scores.update(out_scores)
 
         return out_scores
 
-    def traverse_branch(self, verbose=False):
+    def traverse_branch(self):
 
-        if verbose:
+        if self.verbose > 0:
+            print('-------------')
             print(self)
+            print('-------------')
 
+        # removes the top member of the back mutation sub-types if it's empty
         if self.back_mtypes and not self.back_mtypes[-1][0]:
             self.back_mtypes = self.back_mtypes[:-1]
 
         null_scores = self.get_null_performance()
-        new_scores = self.step(verbose)
+        new_scores = self.step()
 
         # if we have found sub-types of the current mutation type...
         if new_scores:
 
+            # ...finds how functionally similar the classification signatures
+            # of the sub-types are to the signature of the current type
             mtype_proj = self.get_clf_projection(
                 [mtype for mtype, _ in new_scores.items()])
-            print(mtype_proj)
+
+            if self.verbose > 1:
+                print("\nRemaining samples' probability of being labelled "
+                      "positively with each sub-type's classifier:")
+                print('\n'.join(
+                    '\t{}:\t\t{:.1%}'.format(s[0], (1 + 2 ** -s[1]) ** -1)
+                    for s in mtype_proj.items()))
 
             mtype_sizes = [len(mtype.get_samples(self.cdata.train_mut))
                            for mtype, _ in new_scores.items()]
-
 
             null_wghts = [
                 [exp(-(((nl_sz - new_sz) ** 2) / new_sz ** 2)) ** 2
@@ -259,6 +295,14 @@ class PartitionOptim(object):
                 zip(null_wghts, null_means, null_crcts)
                 ]
 
+            if self.verbose > 2:
+                print("\nResults of background classification analysis "
+                      "(mean +/- sd) :")
+
+                print('\n'.join('\t{}:\t\t{:.4f} +/- {:.4f}'.format(*s)
+                                for s in zip(
+                                    new_scores.keys(), null_means, null_sds)))
+
             next_mtypes = sorted(
                 [(mtype, (acc['Null'] - nl_mn) / nl_sd)
                  for (mtype, acc), nl_mn, nl_sd in
@@ -266,25 +310,41 @@ class PartitionOptim(object):
                 key=lambda x: x[1]
                 )
 
+            if self.verbose > 1:
+                print("\nSub-type classification performance z-score "
+                      "relative to background sample subset distribution:")
+                print('\n'.join('\t{}:\t\t{:.2f}'.format(*s)
+                                for s in next_mtypes))
+
             next_mtypes = [x[0] for x in next_mtypes
                            if x[1] > 2 and mtype_proj[x[0]] < 0]
-            print(next_mtypes)
 
             # ...and at least one of these sub-types are significantly
             # better than the current type...
             if next_mtypes:
+                if self.verbose > 1:
+                    print("Found {} further sub-types...".format(
+                        len(next_mtypes)))
 
                 # ...and if we have reached the bottom of the mutation
                 # annotation hierarchy, add these sub-types to the list of
                 # optimal sub-types...
                 if self.lvl_index == (len(self.use_lvls) - 1):
-                    self.best_mtypes += next_mtypes
+                    if self.verbose > 1:
+                        print("\nFinal mutation annotation level reached, "
+                              "adding following sub-types to optimal set:")
+                        print('\n'.join('\t{}'.format(mtype)
+                                        for mtype in next_mtypes))
+
+                    self.best_mtypes |= set(next_mtypes)
 
                     # ...and then go back and interrogate previously found
                     # sub-types that were better than their parent type
                     if self.back_mtypes:
                         self.cur_mtype = self.back_mtypes[-1][0].pop()
-                        return self.traverse_branch(verbose)
+                        self.lvl_index = self.back_mtypes[-1][1]
+
+                        return True
 
                 # ...if we can go deeper in the hierarchy, proceed to test
                 # the first of these better sub-types, saving the rest for
@@ -294,22 +354,24 @@ class PartitionOptim(object):
                     self.back_mtypes += [[next_mtypes, self.lvl_index]]
                     self.lvl_index += 1
 
-                    return self.traverse_branch(verbose)
+                    return True
 
         # ...otherwise check if we can skip this mutation annotation level
         # and go one level deeper to find more sub-types
         if self.lvl_index < (len(self.use_lvls) - 1):
             self.lvl_index += 1
-            return self.traverse_branch(verbose)
+            return True
 
         # ...otherwise check to see if there are sub-types at the current
         # mutation level we can go back to...
         if self.back_mtypes:
-            self.best_mtypes.append(self.cur_mtype)
+            self.best_mtypes |= set(self.cur_mtype)
             self.cur_mtype = self.back_mtypes[-1][0].pop()
             self.lvl_index = self.back_mtypes[-1][1]
 
-            return self.traverse_branch(verbose)
+            return True
+
+        return False
 
 
 def main(argv):
@@ -337,13 +399,8 @@ def main(argv):
     optim = PartitionOptim(cdata, base_mtype, argv[1], eval(argv[2]),
                            ('Form', 'Exon', 'Protein'))
 
-    optim.traverse_branch(verbose=True)
-    print(optim.cur_mtype)
-    print(optim.best_mtypes)
-    print(optim.next_mtypes)
-    print(optim.back_mtypes)
-    print(optim.lvl_index)
-    optim.traverse_branch(verbose=True)
+    while optim.traverse_branch():
+        pass
 
     # saves classifier results to file
     out_file = os.path.join(
