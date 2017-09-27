2340592edaa6630c0d7a3997131932758095119b
estabroj <estabroj@metal-1-0.local>
2017-09-26 21:35:10 -0700

    Add write_regulon and translate to branch

--- HetMan/experiments/tf_activity/translate_regulon.R
+++ HetMan/experiments/tf_activity/translate_regulon.R
@@ -1,5 +1,5 @@
-# a script for translating regulons with ENTREZID identifiers to ENSEMBL. Regulons
-# are generated from aracne.networks package (v.1.2.0).
+# a script for translating regulons with ENTREZID identifiers to ENSEMBL.
+# Regulons are generated from aracne.networks package (v.1.2.0).
 # aracne.networks usage https://bioconductor.org/packages/release/data/experiment/manuals/aracne.networks/man/aracne.networks.pdf
 # Run from command line: Rscript translate_regulon.R --args args1
 # where arg1= TCGA cohort (i.e. TCGA-BRCA)
@@ -21,19 +21,16 @@ getMatrixWithSelectedIds = function(df, columns=list('Regulator','Target'), db='
     return(df2)
 }
 
-print("TEST")
 basedir <- "."
-datadir <- paste(basedir,"/../../data/tf_activity/", sep="/")
+#datadir <- paste(basedir,"/../../data/tf_activity/", sep="/")
+datadir <- paste("/home/exacloud/lustre1/BioCoders/ProjectCollaborations/PRECEPTS/bergamot/HetMan/data/tf_activity")
 
 main = function(){
     setwd(datadir)
-    print(paste("Basedir:", basedir))
-    print(paste("Datadir:", datadir))
     args = commandArgs(trailingOnly = TRUE)
     cohort = (args[1])
-    print(paste("Cohort:", cohort))s
     id = strsplit(cohort,'-')[[1]][2]
-    cohort_table = fread(paste('tmp-entrez-TCGA-', id,'-reg.adj',sep=''))
+    cohort_table = fread(paste('tmp-', id,'.adj',sep=''))
     mappedIDs = getMatrixWithSelectedIds(cohort_table)
     write.table(mappedIDs,paste('tmp-ensembl-',id,'.adj',sep=''),sep='\t',row.names=F,quote=F)
 }
