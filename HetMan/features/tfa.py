278682390a8beb6cfa48e8515bb4a6259b3b4c2f
Joey Estabrook <estabroj@ohsu.edu>
2017-11-22 10:44:27 -0800

    Fixed TypeError : index_col=0 issue

--- HetMan/features/tfa.py
+++ HetMan/features/tfa.py
@@ -12,6 +12,6 @@ def get_tfa_data():
     Args:
 	#todo currently hard coded for BRCA NES signatures, add utility to pull TFA-NES values for all TCGA-cohorts
     """
-    tfa_mat = pd.read_csv('/home/exacloud/lustre1/BioCoders/ProjectCollaborations/PRECEPTS/III/data/BRCA-all-gene-TFA-NES.txt',sep='\t',index=0).transpose()
+    tfa_mat = pd.read_csv('/home/exacloud/lustre1/BioCoders/ProjectCollaborations/PRECEPTS/III/data/BRCA-all-gene-TFA-NES.txt',sep='\t',index_col=0).transpose()
     return tfa_mat
 
