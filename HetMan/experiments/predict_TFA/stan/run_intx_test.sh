593ca13e1e919f45005265242f1ca85f8fe36a1d
Joey Estabrook <estabroj@ohsu.edu>
2017-11-22 10:34:57 -0800

    modified sbatch log file

--- HetMan/experiments/predict_TFA/stan/run_intx_test.sh
+++ HetMan/experiments/predict_TFA/stan/run_intx_test.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
 
-#SBATCH --job-name=dream-stan-intx
+#SBATCH --job-name=TFA-stan-intx
 #SBATCH --partition=exacloud
 #SBATCH --time=20
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
 
-#SBATCH --output=/home/exacloud/lustre1/CompBio/mgrzad/slurm/log-files/dream-stan-intx_%j.out
-#SBATCH --error=/home/exacloud/lustre1/CompBio/mgrzad/slurm/log-files/dream-stan-intx_%j.err
+#SBATCH --output=/home/users/estabroj/scratch/slurm/log-files/TFA-stan-intx_%j.out
+#SBATCH --error=/home/users/estabroj/scratch/slurm/log-files/TFA-stan-intx_%j.err
 #SBATCH --verbose
 
 
-cd ~/compbio/bergamot
-source activate visions
+cd /home/users/estabroj/scratch/bergamot
+source activate paulie
 
 # finds the name of the TCGA cohort to use
 if [ -z ${cohort+x} ]
 then
-	echo "no cohort defined, defaulting to OV"
-	export cohort="OV"
+	echo "no cohort defined, defaulting to BRCA"
+	export cohort="BRCA"
 fi
 
 # gets the output directory where results will be saved,
 # removing it if it already exists
-TEMPDIR=HetMan/experiments/dream_train/stan/output/intx/$cohort
+TEMPDIR=HetMan/experiments/predict_TFA/stan/output/intx/$cohort
 echo $TEMPDIR
 rm -rf $TEMPDIR
 
@@ -35,6 +35,6 @@ mkdir -p $TEMPDIR/results
 intx_list=('controls-expression-of' 'controls-phosphorylation-of' 'controls-state-change-of')
 for intx_use in "${intx_list[@]}";
 	do export intx=$intx_use;
-	sbatch HetMan/experiments/dream_train/stan/run_cv.sh;
+	sbatch HetMan/experiments/predict_TFA/stan/run_cv.sh;
 done;
 
