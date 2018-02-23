27f0c4c39822ad1084eb304a95fd9fc5b9a2280e
Michal Grzadkowski <mgrzad@gmail.com>
2018-02-23 12:26:47 -0800

    Adding first version of fitting basic ICGC transfer models from various TCGA cohorts.

---
+++ HetMan/experiments/icgc_transfer_test/fit_models.py
@@ -0,0 +1,127 @@
+
+import os
+base_dir = os.path.dirname(__file__)
+
+import sys
+sys.path.extend([os.path.join(base_dir, '../../..')])
+
+from HetMan.features.cohorts.tcga import MutationCohort as TCGAcohort
+from HetMan.features.cohorts.icgc import MutationCohort as ICGCcohort
+from HetMan.features.variants import MuType
+from HetMan.predict.basic.classifiers import *
+
+import argparse
+import synapseclient
+from math import ceil
+import dill as pickle
+
+icgc_data_dir = '/home/exacloud/lustre1/share_your_data_here/precepts/ICGC'
+firehose_data_dir = ('/home/exacloud/lustre1/share_your_data_here'
+                     '/precepts/firehose/')
+
+
+def main():
+    """Runs the experiment."""
+
+    parser = argparse.ArgumentParser(
+        description=("Test a classifier's ability to predict the presence "
+                     "of a list of sub-types.")
+        )
+
+    parser.add_argument('classif', type=str,
+                        help='a classifier in HetMan.predict.classifiers')
+    parser.add_argument('cv_id', type=int,
+                        help='a random seed used for cross-validation')
+    parser.add_argument('task_id', type=int,
+                        help='the subset of sub-types to assign to this task')
+
+    parser.add_argument(
+        '--tune_splits', type=int, default=4,
+        help='how many training cohort splits to use for tuning'
+        )
+    parser.add_argument(
+        '--test_count', type=int, default=24,
+        help='how many hyper-parameter values to test in each tuning split'
+        )
+    parser.add_argument(
+        '--parallel_jobs', type=int, default=8,
+        help='how many parallel CPUs to allocate the tuning tests across'
+        )
+
+    parser.add_argument('--verbose', '-v', action='store_true',
+                        help='turns on diagnostic messages')
+
+    args = parser.parse_args()
+    if args.verbose:
+        print("Starting ICGC transfer test with classifier {} for "
+              "cross-validation ID {} and task ID {} ...".format(
+                  args.classif, args.cv_id, args.task_id))
+
+    cohort_genes = sorted(pickle.load(
+        open(os.path.join(base_dir, 'cohort_genes.p'), 'rb')))
+
+    test_count = ceil(len(cohort_genes) / 5)
+    cohort_genes = [x for i, x in enumerate(cohort_genes)
+                    if i // test_count == args.task_id]
+
+    use_cohorts = set(coh for coh, _ in cohort_genes)
+    mut_clf = eval(args.classif)
+    out_acc = {cohort: dict() for cohort in use_cohorts}
+
+    cdata_icgc = ICGCcohort('PACA-AU', icgc_data_dir, mut_genes=None,
+                            samp_cutoff=[1/12, 11/12], cv_prop=1.0)
+
+    syn = synapseclient.Synapse()
+    syn.cache.cache_root_dir = ("/home/exacloud/lustre1/CompBio/"
+                                "mgrzad/input-data/synapse")
+    syn.login()
+    
+    for cohort in use_cohorts:
+        cur_genes = [gn for coh, gn in cohort_genes if coh == cohort]
+
+        tcga_cdata = TCGAcohort(
+            cohort=cohort, mut_genes=cur_genes, mut_levels=['Gene'],
+            expr_source='Firehose', expr_dir=firehose_data_dir,
+            cv_prop=0.75, cv_seed=(args.cv_id - 37) * 101, syn=syn
+            )
+
+        if args.verbose:
+            print("Loaded mutations for {} genes in cohort {} with "
+                  "{} samples.".format(
+                      len(cur_genes), cohort, len(tcga_cdata.samples))
+                    )
+
+        for gene in cur_genes:
+            if args.verbose:
+                print("Testing {} ...".format(gene))
+
+            clf = mut_clf()
+            use_genes = (cdata_icgc.genes & tcga_cdata.genes) - {gene}
+            use_mtype = MuType({('Gene', gene): None})
+
+            clf.tune_coh(tcga_cdata, use_mtype, include_genes=use_genes,
+                         tune_splits=args.tune_splits,
+                         test_count=args.test_count,
+                         parallel_jobs=args.parallel_jobs)
+
+            clf.fit_coh(tcga_cdata, use_mtype, include_genes=use_genes)
+            out_acc[cohort][gene] = clf.eval_coh(
+                cdata_icgc, use_mtype, include_genes=use_genes,
+                use_train=True
+                )
+
+    out_file = os.path.join(base_dir, 'output', args.classif,
+                            'out__cv-{}_task-{}.p'.format(
+                                args.cv_id, args.task_id)
+                            )
+
+    pickle.dump({'Acc': out_acc,
+                 'Info': {'TuneSplits': args.tune_splits,
+                          'TestCount': args.test_count,
+                          'ParallelJobs': args.parallel_jobs}},
+                 open(out_file, 'wb'))
+
+
+if __name__ == "__main__":
+    main()
+
--- HetMan/features/cohorts/icgc.py
+++ HetMan/features/cohorts/icgc.py
@@ -1,10 +1,16 @@
 
+"""Consolidating datasets from the International Cancer Genome Consortium.
+
+Authors: Michal Grzadkowski <grzadkow@ohsu.edu>
+
+"""
+
 from .mut import BaseMutationCohort
-from ..expression import get_expr_icgc, log_norm_expr
+from ..expression import get_expr_icgc
 from ..variants import get_variants_icgc
 
 from ..annot import get_gencode
-from ..utils import match_icgc_samples
+from ..utils import log_norm, match_icgc_samples
 
 
 class MutationCohort(BaseMutationCohort):
@@ -14,18 +20,23 @@ class MutationCohort(BaseMutationCohort):
                  top_genes=100, samp_cutoff=None,
                  cv_prop=2.0/3, cv_seed=None):
 
-        # load expression and mutation data for the given cohort from the
-        # directory where the data was downloaded
+        # load ICGC expression and mutation data for the given cohort from
+        # the given local directory
         expr = get_expr_icgc(cohort, data_dir)
         variants = get_variants_icgc(cohort, data_dir)
 
+        # load gene annotation data, find the genes for which we have both
+        # expression and annotation data
         annot = get_gencode()
         use_genes = annot.keys() & expr.columns
 
+        # remove genes from the expression data for which we do not have
+        # annotation data, log-normalize the expression values
         expr = expr.loc[:, use_genes]
-        expr = log_norm_expr(expr.fillna(expr.min().min()))
+        expr = log_norm(expr.fillna(0.0))
 
-        # load annotation data for the genes in the expression dataset
+        # replace the gene Ensembl IDs with gene names in the expression
+        # data, reformat annotation data to use gene names as keys
         expr.columns = [annot[ens]['gene_name'] for ens in expr.columns]
         gene_annot = {annot[ens]['gene_name']: {**{'Ens': ens}, **annot[ens]}
                       for ens in use_genes}
@@ -35,14 +46,18 @@ class MutationCohort(BaseMutationCohort):
         matched_samps = match_icgc_samples(expr.index, variants['Sample'],
                                            cohort, data_dir)
 
+        # remove entries in the mutation data that are not associated with a
+        # gene for which we have annotation data
         var_df = variants.loc[~variants['Gene'].isnull(), :]
         var_df = var_df.loc[var_df['Gene'].isin(annot.keys()), :]
 
+        # remove irrelevant mutation types
         variants = var_df.loc[~var_df['Form'].isin(
             ['intron_variant', 'upstream_gene_variant',
              'downstream_gene_variant', 'intragenic_variant']
             ), :]
 
+        # replace gene Ensembl IDs with gene names in the mutation data
         new_gns = [annot[gn]['gene_name'] for gn in variants['Gene']] 
         variants = variants.drop(labels=['Gene'], axis="columns",
                                  inplace=False)
@@ -50,3 +65,4 @@ class MutationCohort(BaseMutationCohort):
 
         super().__init__(expr, variants, matched_samps, gene_annot, mut_genes,
                          mut_levels, top_genes, samp_cutoff, cv_prop, cv_seed)
+
