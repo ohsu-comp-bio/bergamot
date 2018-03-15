ed722b5ef95c0476f013f7e2b772769d24d71485
Michal Grzadkowski <mgrzad@gmail.com>
2018-03-15 14:22:15 -0700

    Cleaning up pipeline methods for prediciton and scoring to better accomodate transfer pipelines.

--- HetMan/predict/pipelines.py
+++ HetMan/predict/pipelines.py
@@ -7,8 +7,7 @@ phenotypic information from -omic datasets.
 
 See Also:
     :module:`../features/cohorts`: Storing -omic and phenotypic data.
-    :module:`.classifiers`: Specific algorithms for binary prediction.
-    :module:`.regressors`: Specific algorithms for continuous prediction.
+    :module:`.basic/classifiers`: Specific algorithms for binary prediction.
 
 Author: Michal Grzadkowski <grzadkow@ohsu.edu>
 
@@ -172,11 +171,12 @@ class OmicPipe(Pipeline):
             )
 
     def predict_base(self, omic_data):
+        """Applies the prediction method specific to the pipeline."""
         return self.predict(omic_data)
 
     @staticmethod
     def parse_preds(preds):
-        return preds
+        return np.array(preds).flatten()
 
     def predict_omic(self, omic_data):
         """Gets a vector of phenotype predictions for an -omic dataset."""
@@ -195,7 +195,7 @@ class OmicPipe(Pipeline):
     def extra_tune_params(cls, cohort):
         return cls.extra_fit_params(cohort)
 
-    def score(self, X, y=None):
+    def score(self, X, y=None, sample_weight=None):
         """Get the accuracy of the classifier in predicting phenotype values.
         
         Used to ensure compatibility with cross-validation methods
@@ -208,21 +208,26 @@ class OmicPipe(Pipeline):
             y (array-like), shape = [n_samples, ]
                 A vector of phenotype values.
 
+            sample_weight (array-like), default = None
+                If not None, how much weight to assign to the prediction for
+                each sample when calculating the score.
+
         Returns:
             S (float): A score corresponding to prediction accuracy.
                 The way this score is calculated is determined by the
                 pipeline's `score_pheno` method.
 
         """
-        return self.score_pheno(np.array(y).flatten(),
-                                np.array(self.predict_omic(X)).flatten())
+        return self.score_omic(y, self.predict_omic(X))
+
+    def score_omic(self, actual_omic, pred_omic):
+        """Parses and scores the predictions for a set of phenotypes."""
+        return self.score_pheno(actual_omic.flatten(), pred_omic)
 
     @staticmethod
-    def score_pheno(X, y):
-        """The function used to calculate the accuracy of phenotype values
-           predicted by the pipeline and the actual values themselves."""
-        raise PipelineError("Class `OmicPipe` can't be used for prediction, "
-                            "use its subclasses instead!")
+    @abstractmethod
+    def score_pheno(actual_pheno, pred_pheno):
+        """Scores the predicted values for a single phenotype."""
 
     def tune_coh(self,
                  cohort, pheno,
@@ -364,11 +369,7 @@ class OmicPipe(Pipeline):
                 include_genes, exclude_genes
                 )
 
-        if len(test_pheno) < 5:
-            return float('nan')
-
-        else:
-            return self.score(test_omics, test_pheno)
+        return self.score(test_omics, test_pheno)
 
     def tune_fit_eval(self,
                       cohort, pheno,
@@ -451,18 +452,27 @@ class PresencePipe(OmicPipe):
         return self.predict_proba(omic_data)
 
     @staticmethod
-    def score_pheno(X, y):
-        if len(np.unique(X)) == 1:
-            return 0.5
-        else:
-            return roc_auc_score(X, y)
+    def score_pheno(actual_pheno, pred_pheno):
+        pheno_score = 0.5
+
+        if (len(np.unique(actual_pheno)) > 1
+                and len(np.unique(pred_pheno)) > 1):
+            pheno_score = roc_auc_score(actual_pheno, pred_pheno)
+
+        return pheno_score
 
 
 class ValuePipe(OmicPipe):
     """A class corresponding to pipelines which use continuous data to
        predict continuous outcomes.
     """
-    pass
+
+    @staticmethod
+    def score_pheno(actual_pheno, pred_pheno):
+        if np.var(actual_pheno) == 0 or np.var(pred_pheno) == 0:
+            return 0
+        else:
+            return pearsonr(actual_pheno, pred_pheno)[0]
 
 
 class TransferPipe(OmicPipe):
@@ -497,7 +507,7 @@ class TransferPipe(OmicPipe):
                             .__code__.co_varnames):
                         fit_params_steps[step][pname] = pval
 
-        self.lbl_steps = {lbl: [] for lbl in X_dict}
+        self.lbl_transforms = {lbl: [] for lbl in X_dict}
         Xt_dict = {lbl: None for lbl in X_dict}
 
         for lbl in X_dict:
@@ -517,7 +527,8 @@ class TransferPipe(OmicPipe):
                         **fit_params_steps[name]
                         ).transform(Xt)
 
-            self.lbl_steps[lbl] += [(name, copy(transform))]
+                self.lbl_transforms[lbl] += [(name, copy(transform))]
+
             Xt_dict[lbl] = Xt
 
         if self._final_estimator is None:
@@ -553,77 +564,62 @@ class TransferPipe(OmicPipe):
 
         return self
 
+    def predict_proba(self, X):
+        Xt_dict = X
+
+        for lbl in X:
+            for name, transform in self.lbl_transforms[lbl]:
+
+                if transform is not None:
+                    Xt_dict[lbl] = transform.transform(Xt_dict[lbl])
+
+        return self.steps[-1][-1].predict_proba(Xt_dict)
+
+    @staticmethod
+    def parse_preds(preds):
+        return {lbl: np.array(x).flatten() for lbl, x in preds.items()}
+
+    def score_omic(self, actual_omic, pred_omic):
+        """Parses and scores the predictions for a set of phenotypes."""
+        return np.min([self.score_pheno(actual_omic[lbl].flatten(),
+                                        pred_omic[lbl])
+                       for lbl in actual_omic])
+
 
 class MultiPipe(OmicPipe):
     """A pipeline for predicting multiple phenotypes at once.
 
     """
 
-    def predict_omic(self, omic_data):
-        return [self.parse_preds(preds)
-                for preds in self.predict_base(omic_data)]
-
-    def score(self, X, y=None):
-        y_pred = np.array(self.predict_omic(X))
-
-        if y.shape[0] == X.shape[0]:
-            y = np.transpose(y)
-        
-        if y_pred.shape[0] == X.shape[0]:
-            y_pred = np.transpose(y_pred)
-
-        return self.score_pheno(y.tolist(), y_pred.tolist())
-
-    def score_pheno(self, ylist_true, ylist_pred):
-        return self.parse_scores(
-            [self.score_each(true_y, pred_y)
-             for true_y, pred_y in zip(ylist_true, ylist_pred)]
-            )
-
     @staticmethod
-    def parse_scores(scores):
-        """Summarizes the scores across the predicted variates."""
-        return np.min(scores)
+    def parse_preds(preds):
+        pass
 
-    @staticmethod
-    def score_each(y_true, y_pred):
-        return pearsonr(y_true, y_pred)[0]
+    def score_omic(self, actual_omic, pred_omic):
+        return np.mean([self.score_pheno(act_omic, p_omic)
+                        for act_omic, p_omic in zip(actual_omic, pred_omic)])
 
 
 class ProteinPipe(ValuePipe):
     """A class corresponding to pipelines for predicting proteomic levels."""
-    
+
     @staticmethod
-    def score_pheno(X, y):
-        if np.var(X) == 0 or np.var(y) == 0:
+    def score_pheno(actual_pheno, pred_pheno):
+        if np.var(actual_pheno) == 0 or np.var(pred_pheno) == 0:
             return 0
         else:
-            return pearsonr(X, y)[0]
+            return r2_score(actual_pheno, pred_pheno)
 
 
-class DrugPipe(ValuePipe):
-
-    @staticmethod
-    def score_pheno(X, y):
-        return r2_score(X, y, multioutput='variance_weighted')
-
-
-class VariantPipe(PresencePipe):
-    """A class corresponding to pipelines for predicting discrete gene
-       mutation states such as SNPs, indels, and frameshifts.
+class MutPipe(PresencePipe):
+    """A class corresponding to pipelines for predicting
+       discrete gene mutation states individually.
     """
 
     def __repr__(self):
         """Prints the classifier name and the feature selection path key."""
         return '{}_{}'.format(
             type(self).__name__, str(self.get_params()['path_keys']))
-
-
-class MutPipe(VariantPipe):
-    """A class corresponding to pipelines for predicting
-       discrete gene mutation states individually.
-    """
-    pass
 
 
 class LinearPipe(OmicPipe):
