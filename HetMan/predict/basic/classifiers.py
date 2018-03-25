112f1ee3c72ee80d97452fe2c6acbf678f283487
Michal Grzadkowski <mgrzad@gmail.com>
2018-03-25 11:08:41 -0700

    Removing pathway feature selection from basic classifiers, cleaning up classifier code.

--- HetMan/predict/basic/classifiers.py
+++ HetMan/predict/basic/classifiers.py
@@ -1,64 +1,41 @@
 
-"""Specific pipelines for prediction of binary phenotypes.
-
-See Also:
-    :module:`..pipelines`: Base classes for phenotype prediction.
+"""Simple classifiers from predicting -omic phenotypes.
 
 Author: Michal Grzadkowski <grzadkow@ohsu.edu>
 
 """
 
-from ..pipelines import MutPipe, LinearPipe, EnsemblePipe
-from ..selection import *
+from ..pipelines import PresencePipe, LinearPipe, EnsemblePipe
 
 from math import exp
 from scipy import stats
 
-from sklearn.feature_selection import VarianceThreshold, SelectPercentile
-from sklearn.preprocessing import StandardScaler, RobustScaler
-
+from sklearn.preprocessing import StandardScaler
 from sklearn.naive_bayes import GaussianNB
 from sklearn.linear_model import LogisticRegression, SGDClassifier
 from sklearn.svm import SVC
-from sklearn.ensemble import (RandomForestClassifier,
-                              GradientBoostingClassifier)
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.gaussian_process import GaussianProcessClassifier
 
 
-class NaiveBayes(MutPipe):
+class BasicPipe(PresencePipe):
+
+    def __init__(self, fit_inst):
+        super().__init__([('norm', StandardScaler()), ('fit', fit_inst)])
+
+
+class NaiveBayes(BasicPipe):
     """A class corresponding to Gaussian Naive Bayesian classification
        of mutation status.
     """
 
-    def __init__(self, path_keys=None):
-        feat_step = PathwaySelect(path_keys=path_keys)
-        norm_step = StandardScaler()
-        fit_step = GaussianNB()
-        MutPipe.__init__(
-            self,
-            [('feat', feat_step), ('norm', norm_step), ('fit', fit_step)],
-            path_keys
-            )
+    def __init__(self):
+        super().__init__(GaussianNB())
 
 
-class RobustNB(MutPipe):
-    """A class corresponding to Gaussian Naive Bayesian classification
-       of mutation status with robust feature scaling.
-    """
-
-    def __init__(self, path_keys=None):
-        feat_step = PathwaySelect(path_keys=path_keys)
-        norm_step = RobustScaler()
-        fit_step = GaussianNB()
-
-        super(RobustNB, self).__init__(
-            [('feat', feat_step), ('norm', norm_step), ('fit', fit_step)],
-            path_keys=path_keys
-            )
-
-
-class Lasso(MutPipe, LinearPipe):
+class Lasso(BasicPipe, LinearPipe):
     """A class corresponding to logistic regression classification
        of mutation status with the lasso regularization penalty.
     """
@@ -67,179 +44,119 @@ class Lasso(MutPipe, LinearPipe):
         ('fit__C', stats.lognorm(scale=exp(-1), s=exp(1))),
         )
 
-    def __init__(self, path_keys=None):
-        feat_step = PathwaySelect(path_keys=path_keys)
-        norm_step = StandardScaler()
-        fit_step = LogisticRegression(penalty='l1', tol=1e-2,
-                                      class_weight='balanced')
-
-        super().__init__(
-            [('feat', feat_step), ('norm', norm_step),('fit', fit_step)],
-            path_keys=path_keys
-            )
+    def __init__(self):
+        super().__init__(LogisticRegression(penalty='l1', max_iter=500,
+                                            class_weight='balanced'))
 
 
-class ElasticNet(MutPipe, LinearPipe):
-    """A class corresponding to logistic regression classification
-       of mutation status with the elastic net regularization penalty.
-    """
-
-    tune_priors = (
-        ('fit__alpha', stats.lognorm(scale=exp(1), s=exp(1))),
-        ('fit__l1_ratio', (0.05, 0.25, 0.5, 0.75, 0.95))
-        )
-
-    def __init__(self, path_keys=None):
-        feat_step = PathwaySelect(path_keys=path_keys)
-        norm_step = StandardScaler()
-        fit_step = SGDClassifier(loss='log', penalty='elasticnet',
-                                 max_iter=500, class_weight='balanced')
-
-        super().__init__(
-            [('feat', feat_step), ('norm', norm_step), ('fit', fit_step)],
-            path_keys=path_keys
-            )
-
-
-class Ridge(MutPipe, LinearPipe):
+class Ridge(BasicPipe, LinearPipe):
     """A class corresponding to logistic regression classification
        of mutation status with the ridge regularization penalty.
     """
 
     tune_priors = (
         ('fit__C', stats.lognorm(scale=exp(-1), s=exp(1))),
-    )
+        )
 
-    def __init__(self, path_keys=None):
-        feat_step = PathwaySelect(path_keys=path_keys)
-        norm_step = StandardScaler()
-        fit_step = LogisticRegression(penalty='l2', tol=1e-3,
-                                      class_weight='balanced')
-
-        super().__init__([('feat', feat_step),
-                          ('norm', norm_step), ('fit', fit_step)],
-                         path_keys=path_keys)
+    def __init__(self):
+        super().__init__(LogisticRegression(penalty='l2', max_iter=1000,
+                                            class_weight='balanced'))
 
 
-class SVCpoly(MutPipe):
+class ElasticNet(BasicPipe, LinearPipe):
+    """A class corresponding to logistic regression classification
+       of mutation status with the elastic net regularization penalty.
+    """
+
+    tune_priors = (
+        ('fit__alpha', stats.lognorm(scale=exp(1), s=exp(1))),
+        ('fit__l1_ratio', (0.05, 0.25, 0.5, 0.75, 0.95)),
+        )
+
+    def __init__(self):
+        super().__init__(
+            SGDClassifier(loss='log', penalty='elasticnet',
+                          max_iter=1000, class_weight='balanced')
+            )
+
+
+class SVCpoly(BasicPipe):
     """A class corresponding to C-support vector classification
        of mutation status with a radial basis kernel.
     """
    
     tune_priors = (
         ('fit__C', stats.lognorm(scale=exp(-1), s=exp(1))),
-        ('fit__coef0', [-2., -1., -0.5, 0., 0.5, 1., 2.]),
-    )
+        ('fit__coef0', (-5., -2., -1., -0.5, 0., 0.5, 1., 2., 5.)),
+        ('fit__degree', (2, 3, 4, 5)),
+        )
 
-    def __init__(self, path_keys=None):
-        feat_step = PathwaySelect(path_keys=path_keys)
-        norm_step = StandardScaler()
-        fit_step = SVC(
-            kernel='poly', probability=True, degree=2,
-            cache_size=500, class_weight='balanced')
-        MutPipe.__init__(
-            self,
-            [('feat', feat_step), ('norm', norm_step), ('fit', fit_step)],
-            path_keys
-            )
+    def __init__(self):
+        super().__init__(SVC(kernel='poly', probability=True,
+                             cache_size=500, class_weight='balanced'))
 
 
-class SVCrbf(MutPipe):
+class SVCrbf(BasicPipe):
     """A class corresponding to C-support vector classification
        of mutation status with a radial basis kernel.
     """
    
     tune_priors = (
         ('fit__C', stats.lognorm(scale=exp(-1), s=exp(2))),
-        ('fit__gamma', stats.lognorm(scale=1e-4, s=exp(2)))
+        ('fit__gamma', stats.lognorm(scale=1e-4, s=exp(2))),
         )
 
-    def __init__(self, path_keys=None):
-        feat_step = PathwaySelect(path_keys=path_keys)
-        norm_step = StandardScaler()
-        fit_step = SVC(kernel='rbf', probability=True,
-                       cache_size=500, class_weight='balanced')
-
-        super().__init__(
-            [('feat', feat_step), ('norm', norm_step),('fit', fit_step)],
-            path_keys=path_keys
-            )
+    def __init__(self):
+        super().__init__(SVC(kernel='rbf', probability=True,
+                             cache_size=500, class_weight='balanced'))
 
 
-class GradBoost(MutPipe, EnsemblePipe):
+class GradBoost(BasicPipe, EnsemblePipe):
     """A class for classification using an additive ensemble of trees."""
     
     tune_priors = (
         ('fit__max_depth', (2, 3, 4, 5, 8)),
-        ('fit__min_samples_split', (2, 4, 6, 10)),
+        ('fit__min_samples_split', (0.005, 0.01, 0.02, 0.03, 0.04, 0.05)),
         )
 
-    def __init__(self, path_keys=None):
-        feat_step = PathwaySelect(path_keys=path_keys)
-        norm_step = StandardScaler()
-        fit_step = GradientBoostingClassifier(n_estimators=200)
-
-        super().__init__(
-            [('feat', feat_step), ('norm', norm_step), ('fit', fit_step)],
-            path_keys
-            )
+    def __init__(self):
+        super().__init__(GradientBoostingClassifier(n_estimators=200))
 
 
-class rForest(MutPipe, EnsemblePipe):
+class RandomForest(BasicPipe, EnsemblePipe):
     """A class corresponding to random forest classification
        of mutation status.
     """
 
     tune_priors = (
-        ('fit__max_features', (0.005, 0.01, 0.02, 0.04, 0.08, 0.15)),
-        ('fit__min_samples_leaf', (0.0001, 0.01, 0.05))
+        ('fit__max_features', (0.01, 0.02, 0.04, 0.08, 0.15, "sqrt", "log2")),
+        ('fit__min_samples_leaf', (0.0001, 0.005, 0.01, 0.02, 0.03, 0.05)),
         )
 
-    def __init__(self, path_keys=None):
-        feat_step = PathwaySelect(path_keys=path_keys)
-        norm_step = StandardScaler()
-        fit_step = RandomForestClassifier(
-                    n_estimators=500, class_weight='balanced')
-
-        super().__init__(
-            [('feat', feat_step), ('norm', norm_step), ('fit', fit_step)],
-            path_keys
-            )
+    def __init__(self):
+        super().__init__(RandomForestClassifier(n_estimators=500,
+                                                class_weight='balanced'))
 
 
-class KNeigh(MutPipe):
+class KNeigh(BasicPipe):
     """A class corresponding to k-nearest neighbours voting classification
        of mutation status.
     """
     
     tune_priors = (
         ('fit__n_neighbors', (4, 8, 16, 25, 40)),
-    )
+        )
 
-    def __init__(self, path_keys=None):
-        feat_step = PathwaySelect(path_keys=path_keys)
-        norm_step = StandardScaler()
-        fit_step = KNeighborsClassifier(
-            weights='distance', algorithm='ball_tree')
-
-        super().__init__(
-            [('feat', feat_step), ('norm', norm_step), ('fit', fit_step)],
-            path_keys
-            )
+    def __init__(self):
+        super().__init__(KNeighborsClassifier(weights='distance',
+                                              algorithm='ball_tree'))
 
 
-class GBCrbf(MutPipe):
+class GBCrbf(BasicPipe):
     """A class corresponding to gaussian process classification
        of mutation status with a radial basis kernel.
     """
 
-    def __init__(self, path_keys=None):
-        feat_step = PathwaySelect(path_keys=path_keys)
-        norm_step = StandardScaler()
-        fit_step = GaussianProcessClassifier()
-        MutPipe.__init__(
-            self,
-            [('feat', feat_step), ('norm', norm_step), ('fit', fit_step)],
-            path_keys
-            )
+    def __init__(self):
+        super().__init__(GaussianProcessClassifier())
 
