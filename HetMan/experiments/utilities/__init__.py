7b49a5653ba7e82a08cdc009d680579b82868561
Michal Grzadkowski <mgrzad@gmail.com>
2018-05-11 16:15:14 -0700

    Moving subtype enumeration from experiments to mutation trees, adding mutation cross-testing and dynamic copy-number levels in trees.

--- HetMan/experiments/utilities/__init__.py
+++ HetMan/experiments/utilities/__init__.py
@@ -1,8 +1,6 @@
 
-from .test_cohort_mutypes import load_output as test_output
-from .depict_cohort_mutypes import load_output as depict_output
-from .comb_cohort_mutypes import load_output as comb_output
-from .add_cohort_mutypes import load_output as add_output
+from .isolate_cohort_mutypes import load_output as iso_output
+from .cross_cohort_mutypes import load_output as cross_output
 
-__all__ = ['test_output', 'depict_output', 'comb_output', 'add_output']
+__all__ = ['iso_output', 'cross_output']
 
