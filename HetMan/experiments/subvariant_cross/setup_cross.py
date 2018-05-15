e19ab262681e5afb9e437a035d1916b3cd63d463
Michal Grzadkowski <mgrzad@gmail.com>
2018-05-15 13:42:15 -0700

    Adding experiment to isolate pairs of subtypes in tandem, harmonizing paired- and single-isolation code.

--- HetMan/experiments/subvariant_cross/setup_cross.py
+++ HetMan/experiments/subvariant_cross/setup_cross.py
@@ -1,5 +1,5 @@
 
-"""Enumerating the subtypes of a gene in a cohort to be isolated.
+"""Enumerating the pairs of subtypes of a gene in a cohort to be isolated.
 
 """
 
@@ -14,6 +14,7 @@ from HetMan.features.mutations import MuType
 
 import argparse
 import synapseclient
+from itertools import combinations as combn
 import dill as pickle
 
 firehose_dir = "/home/exacloud/lustre1/share_your_data_here/precepts/firehose"
@@ -21,17 +22,17 @@ firehose_dir = "/home/exacloud/lustre1/share_your_data_here/precepts/firehose"
 
 def main():
     parser = argparse.ArgumentParser(
-        "Set up the gene subtype expression effect isolation experiment by "
-        "enumerating the subtypes to be tested."
+        "Set up the gene subtype expression effect cross-isolation "
+        "experiment by enumerating the pairs of subtypes to be tested."
         )
 
     parser.add_argument('cohort', type=str, help="which TCGA cohort to use")
     parser.add_argument('gene', type=str, help="which gene to consider")
 
     parser.add_argument('mut_levels', type=str,
-                        help="the mutation property levels to consider")
+                        help='the mutation property levels to consider')
 
-    parser.add_argument('--samp_cutoff', type=int, default=20,
+    parser.add_argument('--samp_cutoff', type=int, default=25,
                         help='subtype sample frequency threshold')
 
     parser.add_argument('--verbose', '-v', action='store_true',
@@ -60,38 +61,43 @@ def main():
               "annotation levels {}.\n".format(
                   args.gene, args.samp_cutoff, args.cohort, use_lvls)
              )
-    
-    iso_mtypes = cdata.train_mut.find_unique_subtypes(
-        max_types=2000, max_combs=10, verbose=2,
+
+    cross_mtypes = cdata.train_mut.find_unique_subtypes(
+        max_types=100, max_combs=10, verbose=2,
         sub_levels=use_lvls, min_type_size=args.samp_cutoff
         )
 
-    iso_mtypes = {mtype for mtype in iso_mtypes
-                  if (len(mtype.get_samples(cdata.train_mut))
-                      <= (len(cdata.samples) - args.samp_cutoff))}
+    cross_mtypes = {mtype for mtype in cross_mtypes
+                    if (len(mtype.get_samples(cdata.train_mut))
+                        <= (len(cdata.samples) - args.samp_cutoff))}
 
     if args.verbose:
-        print("\nFound {} total sub-types to isolate!".format(
-            len(iso_mtypes)))
+        print("\nFound {} total sub-types to cross!".format(
+            len(cross_mtypes)))
 
-    use_mtypes = sorted(MuType({('Gene', args.gene): mtype})
-                        for mtype in iso_mtypes)
+    use_pairs = sorted(
+        (mtype1, mtype2) for mtype1, mtype2 in combn(cross_mtypes, 2)
+        if (mtype1 & mtype2).is_empty()
+        )
+
+    if args.verbose:
+        print("\nFound {} non-overlapping sub-type pairs!".format(len(use_pairs)))
 
     # save the list of found non-duplicate sub-types to file
     pickle.dump(
-        use_mtypes,
+        use_pairs,
         open(os.path.join(out_path,
-                          'mtypes_list__samps_{}__levels_{}.p'.format(
+                          'pairs_list__samps_{}__levels_{}.p'.format(
                               args.samp_cutoff, args.mut_levels)),
              'wb')
         )
 
     with open(os.path.join(out_path,
-                           'mtypes_count__samps_{}__levels_{}.txt'.format(
+                           'pairs_count__samps_{}__levels_{}.txt'.format(
                                args.samp_cutoff, args.mut_levels)),
               'w') as fl:
 
-        fl.write(str(len(use_mtypes)))
+        fl.write(str(len(use_pairs)))
 
 
 if __name__ == '__main__':
