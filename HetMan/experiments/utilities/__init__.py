6cb880c3134481e7a9edc4eeb2671f4cd0fcdaa1
Michal Grzadkowski <mgrzad@gmail.com>
2018-05-14 17:42:56 -0700

    Refactoring how AUCs are calculated in isolated subtype ordering and how subtypes are found, starting experiment to cross-isolate subtypes of gene pairs.

--- HetMan/experiments/utilities/__init__.py
+++ HetMan/experiments/utilities/__init__.py
@@ -2,5 +2,31 @@
 from .isolate_cohort_mutypes import load_output as iso_output
 from .cross_cohort_mutypes import load_output as cross_output
 
-__all__ = ['iso_output', 'cross_output']
+from matplotlib.colors import LinearSegmentedColormap
+
+
+cdict = {
+    'red': ((0.0,  2.0/3, 2.0/3),
+            (1.0/3,  1.0, 1.0),
+            (0.5,  0.224, 0.224),
+            (2.0/3,  0.0, 0.0),
+            (1.0,  0.392, 0.392)),
+    
+    'green': ((0.0,  0.329, 0.329),
+              (1.0/3,  1.0, 1.0),
+              (0.5,  0.191, 0.191),
+              (2.0/3,  0.0, 0.0),
+              (1.0,  0.729, 0.729)),
+ 
+    'blue': ((0.0,  0.31, 0.31),
+             (1.0/3,  1.0, 1.0),
+             (0.5,  0.451, 0.451),
+             (2.0/3,  0.0, 0.0),
+             (1.0,  0.416, 0.416))
+    }
+
+simil_cmap = LinearSegmentedColormap('SimilCmap', cdict)
+
+
+__all__ = ['iso_output', 'cross_output', 'simil_cmap']
 
